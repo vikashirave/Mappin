b84c359818cfb4ae6f492eb36d1c90cb8e489c42
Vikashirave <vikashirave@gmail.com>
2022-01-01 12:38:52 +0530

    Created user register and login API

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,21 +1,44 @@
 const router = require("express").Router();
-const Pin = require("../models/Pin");
+const User = require("../models/User");
 const { route } = require("./pins");
+const bcrypt = require("bcrypt");
 
 //register
-route.post("/register", async(req,res)=>{
-    try{
-        //generate new password
+router.post("/register", async (req, res) => {
+  try {
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-        //create new user
-
-        //save user and send response
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
-
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    //save user and send response
+    const user = await newUser.save();
+    res.status(200).json(user._id);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //login
 
-module.exports = router
+router.post("/login", async (req, res) => {
+
+    //find user
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("wrong username or password");
+    //validate password
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("Wrong username or password!")
+    //send response
+    res.status(200).json({_id:user._id, username: user.username});
+ 
+});
+module.exports = router;
